29eed7284e2bf77028a3d088a36c358a24ebfef8
Lovish <lovish@gkmit.co>
2025-11-18 11:55:38 +0530

    feat: set up complete authentication module

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import authRoutes from './routes/authRoutes.js';
+
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:3000', 'http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => res.json({ status: 'ok' }));
 
