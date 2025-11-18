7fffa2a7effa662f2711cdc1e898816f49342ca8
Lovish <lovish@gkmit.co>
2025-11-18 12:07:58 +0530

    fix: removed unwanted localhost url

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: ['http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json());
